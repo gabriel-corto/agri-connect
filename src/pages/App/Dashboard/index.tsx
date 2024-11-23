182bf21ae65215c1a07d921759b25b00ff96917c
gabriel-corto <digital.gabriel2006@gmail.com>
2024-11-23 22:37:52 +0100

    feat: add dashboard ui

--- src/pages/App/Dashboard/index.tsx
+++ src/pages/App/Dashboard/index.tsx
@@ -1,13 +1,20 @@
-import { Truck } from "phosphor-react"
-import { DashboardContainer } from "./style"
+import { CalendarPlus, Truck } from "phosphor-react"
+import { DashboardContainer, DashHeaderContainer } from "./style"
 
 export function Dashboard() {
   return (
     <DashboardContainer>
-      <h3>
-        <Truck size={42}/>
-        <span>Entregas</span>
-      </h3>
+      <DashHeaderContainer>
+        <h3>
+          <Truck size={42}/>
+          <span>Entregas</span>
+        </h3>
+
+        <button>
+          <CalendarPlus size={24} />
+          <span>Agendar Nova Entrega</span>
+        </button>
+      </DashHeaderContainer>
       
       <table>
         <thead>
@@ -22,11 +29,39 @@ export function Dashboard() {
 
         <tbody>
           <tr>
-            <td>Maçã</td>
-            <td>10kg</td>
+            <td>Banana Holandesa</td>
+            <td>32 kg</td>
             <td>MobiTrans</td>
             <td>Há 1 Mês</td>
             <td>Pendente</td>
+          </tr>
+          <tr>
+            <td>Tomate Verde</td>
+            <td>25 kg</td>
+            <td>TransPorto</td>
+            <td>Há 3 Mês</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Maçã Angolanas</td>
+            <td>10kg</td>
+            <td>TransMove</td>
+            <td>Há 1 mês</td>
+            <td>Confirmado</td>
+          </tr>
+          <tr>
+            <td>Cebola Rocha</td>
+            <td>19kg</td>
+            <td>MobiTrans</td>
+            <td>Há 3 meses</td>
+            <td>Pendente</td>
+          </tr>
+          <tr>
+            <td>Ovos</td>
+            <td>60 kg</td>
+            <td>TransLogística</td>
+            <td>Há 1 Mês</td>
+            <td>Confirmado</td>
           </tr>
         </tbody>
       </table>
--- src/pages/App/Dashboard/style.ts
+++ src/pages/App/Dashboard/style.ts
@@ -1,16 +1,77 @@
 import styled from "styled-components";
 
+export const DashHeaderContainer = styled.div`  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    background: ${props => props.theme["green-300"]};
+    color: #fff;
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    font-weight: bold;
+
+    &:hover {
+      cursor: pointer;
+      background: ${props => props.theme["green-500"]};
+    }
+  }
+`
 export const DashboardContainer = styled.div`
   h3 {
     display: flex;
     align-items: center;
     gap: 0.5rem;
     font-size: 1.5rem;
-    color: ${props => props.theme["gray-900"]};
+
   }
 
   table {
     margin-top: 3rem;
-    
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+    box-shadow: 0px 2px 4px #d1d5db;
+
+    th {
+      background: #4b5563;
+      color: #fff;
+      padding: 1rem;
+      text-align: left;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      border-top: 1px solid #9ca3af;
+      padding: 1rem;
+      font-size: 0.875rem;
+      font-weight: 500;
+      line-height: 1.6;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        width: 50% ;
+      }
+      &:last-child {
+        padding-right: 1.5rem;
+      } 
+    }
   }
 `
